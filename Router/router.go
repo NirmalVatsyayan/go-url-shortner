3c1a77a96173be82628abd7cc2d3283e1771d3f4
Nirmal Vatsyayan <nirmal.vatsyayan1990@gmail.com>
2017-06-29 14:56:08 +0530

    updated collection and route name changes

--- Router/router.go
+++ Router/router.go
@@ -11,30 +11,33 @@ func Routes(port string) *gin.Engine{
 	router := gin.New()
 	router.Use(gin.Logger())
 	router.Use(gin.Recovery())
-	router.POST("/login", handler.LoginHandler)
-	router.POST("/register", handler.RegisterHandler)
-	router.GET("/url/:encodedUrl", handler.EncodedUrlRetriveHandler)
 
-	auth := router.Group("/auth")
+	apis := router.Group("/apis/")
+
+	apis.POST("/login", handler.LoginHandler)
+	apis.POST("/register", handler.RegisterHandler)
+	apis.GET("/redirect-url/:encodedUrl", handler.EncodedUrlRetriveHandler)
+
+
+	auth := apis.Group("/auth")
 	auth.Use(middleware.Auth())
 	{
 		auth.GET("/hello", handler.HelloHandler)
 		auth.GET("/refresh_token", handler.RefreshHandler)
 	}
 
-	urlShortner := router.Group("/url-shortner")
-	urlShortner.Use(middleware.Auth())
-	{
-		urlShortner.POST("/submit", handler.UrlPostHandler)
-	}
-
-	authorized := router.Group("/user/")
+	authorized := apis.Group("/user/")
 	authorized.Use(middleware.Auth())
 	{
 		authorized.GET("/profile", handler.ProfileHandler)
-		authorized.GET("urls", handler.GetUserUrlsHandler)
-		authorized.GET("urlinfo/:encodedUrl", handler.GetUrlInfoHandler)
 	}
 
+	shortner := apis.Group("/shortner/")
+	shortner.Use(middleware.Auth())
+	{
+		shortner.POST("submit", handler.UrlPostHandler)
+		shortner.GET("urls", handler.GetUserUrlsHandler)
+		shortner.GET("urls/:encodedUrl", handler.GetUrlInfoHandler)
+	}
 	return router
 }
